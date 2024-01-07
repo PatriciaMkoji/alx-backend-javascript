8d8eab7036d196cacbe883be71db768cfab7eba5
PatriciaMkoji <keyshotrisha@gmail.com>
2024-01-07 13:58:27 +0300

    3. Parameter defaults

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,6 @@
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+	  (expansion1989 === undefined) ? 89:
+	  (expansion2019 === undefined) ? 19:
+
+	  return initialNumber + expansion1989 + expansion2019;
+}
